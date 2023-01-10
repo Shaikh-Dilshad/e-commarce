2b1f2eeaed3ce1967c8112e4cc8d7a3c7eb26d89
dilshad9326 <109910375+dilshad9326@users.noreply.github.com>
2023-01-10 23:40:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-let bar = document.getElementById("bar1")
-let nav = document.getElementById("navheader")
-if(bar){
- bar.addEventListener("onclick " , ()=>{
-    nav.classList.add("active")
- })   
-}
+let mainimg2 = document.getElementById("mainimg")
+
+Array.from(document.getElementsByClassName('sub1')).forEach(element => {
+// let anotherimg2 = document.getElementsByClassName("sub1")
+let anotherimg = element.getElementsByClassName("sub3")
+   anotherimg[0].onclick = ()=>{
+      mainimg2.src = anotherimg[0].src
+   }
+});
+
+ 
+ Array.from(document.getElementsByClassName('pro-continer')).forEach(element => {
+   let f1 =element.getElementsByClassName('f1')
+   f1[0].onclick = ()=>{
+      f1[0] =window.location.href = 'sproduct.html'
+   }
+ });
+
